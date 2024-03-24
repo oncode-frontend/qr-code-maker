2e60ede464bfacf001ac38988e1b8502595f837d
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-24 11:14:02 +0330

    update warn feat

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const $ = document
 const inputElem = $.querySelector('input')
 const btnElem = $.querySelector('.btn-gen')
 const imgElem = $.querySelector('.qr-img')
-
+const warnElem = $.querySelector('.warn')
 const request = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="
 
 btnElem.addEventListener('click', ()=>{
     if (inputElem.value === "") {
-        
+        warnElem.innerHTML = "Fill input!"
+        warnElem.style.color = "red"
+        warnElem.style.textAlign = "center"
+        setTimeout(() => {
+            warnElem.innerHTML = ""
+        }, 2000);
     } else{
         let inputValue = inputElem.value.trim()
         let qrUrl = request + inputValue
